e411ddf0eae411013b6d6fad5399a8f9d5b1994f
시나 <SinaMafuyu@SinaMafuyu.net>
2016-05-06 21:00:13 +0900

    불필요한 파일 삭제

--- Original Data.jmp/data/script/login/main.lua
+++
@@ -1,8 +0,0 @@
-include("../Data/Script/Common/include.lua")
-include("../Data/Script/Common/window.lua")
-include("../Data/Script/Login/gamedef.lua")
-include("../Data/Script/Login/game_loginUI.lua")
-
-function InitUI(wnd)
-	show_login_ui(wnd,1)
-end
